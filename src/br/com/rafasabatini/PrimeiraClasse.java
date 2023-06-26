f2f6beacc422d372882d6b3af7c3c8c4f56315d5
Rafael Sabatini <rafa.sabatini@yahoo.com.br>
2023-06-26 00:26:08 -0300

    primeiro commit

---
+++ src/br/com/rafasabatini/PrimeiraClasse.java
@@ -0,0 +1,10 @@
+package br.com.rafasabatini;
+
+public class PrimeiraClasse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello world");
+	}
+
+}
